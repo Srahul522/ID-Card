ae21249f656821a2dfd46ded95827413b2a0a6ff
Srahul522 <wadan29433@gmail.com>
2025-09-20 10:51:28 +0000

    script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ alert ("Welcome!")
 let rate = confirm ("Hello! you want to rate my work 🤗")
 if (rate){
     prompt("How many in out of 10")
-    alert ("Thanks for rating." + rate + "!")
-    
+    alert ("Thanks for rating.")  
 }
 else {
     alert("Don't Worrie!, you'r Welcome.")
